d25a027e2cdbd6d40bfe97a7f42ce6923fcb6d08
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2021-07-16 22:59:31 +0900

    Create UserApiController.java

---
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -0,0 +1,27 @@
+package com.cos.blog.controller.api;
+
+import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
+import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/user")
+    public ResponseDto<Integer> save(@RequestBody User user) {
+        System.out.println("UserApiController save 호출됨");
+        //실제 DB에 insert 하고 아래 리턴을한다.
+        user.setRole(RoleType.USER);
+        userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+}
