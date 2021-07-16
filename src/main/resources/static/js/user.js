7e39356c8afe317406b973d8c7e1e6f375c3203c
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2021-07-16 22:53:24 +0900

    Modify ajax

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -11,11 +11,24 @@ let index ={
              username: $("#username").val(),
              password: $("#password").val(),
              email: $("#email").val()
-        }
+        };
 
-        // console.log(data);
 
-        $.ajax().done().fail(); //ajax 통신을 이용해서 3개의 파라미터를 json으로 변경하여 insert 요청
+        //ajax default가 비동기 호출
+        //ajax 통신을 이용해서 3개의 파라미터를 json으로 변경하여 insert 요청
+        $.ajax({
+            type:"POST",
+            url:"/blog/api/user",
+            data: JSON.stringify(data), //http body데이터
+            contentType:"application/json; charset=utf-8", //MIME
+            dataType:"json" //요청을 서버로해서 응답이 왔을 때 기본적으로 문자열으로 온다 (json -> js)
+        }).done(function(resp){
+            alert("회원가입이 완료되었습니다.");
+            // console.log(resp);
+            location.href = "/blog";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
     }
 }
 
