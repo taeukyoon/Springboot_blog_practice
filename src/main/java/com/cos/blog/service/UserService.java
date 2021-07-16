7987a9c1168bd761f950dc39f2a2d7ca4607b10b
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2021-07-16 22:54:45 +0900

    Add userService
    UserService부분 추가

---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,21 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입(User user) {
+        userRepository.save(user);
+    }
+
+}
